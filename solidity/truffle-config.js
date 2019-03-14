f71c665bbeacd0d81c2e6b171166fdd2949ea1cf
MarkCBall <moball@hotmail.com>
2019-03-14 10:56:51 -0400

    change compiler to 0.5.0

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -4,7 +4,19 @@ module.exports = {
   // You can also follow this format for other networks;
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
-  /*
+  
+
+
+  compilers: {
+        solc: {
+            version: "0.5.0",
+            optimizer: {
+                enabled: true,
+                runs: 5000000
+            }
+        }
+    },
+/*
   networks: {
     development: {
       host: "127.0.0.1",
