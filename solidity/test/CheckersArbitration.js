adab3ab721d0b16ec8e4c344f407073ed3eb2ecd
MarkCBall <moball@hotmail.com>
2019-03-20 15:46:20 -0400

    test placeholders done

--- solidity/test/CheckersArbitration.js
+++ solidity/test/CheckersArbitration.js
@@ -6,9 +6,9 @@ const truffleAssert = require('truffle-assertions');
 const SigLib = require("../Library/SigLib")
 
 contract('StateChGaming', async (accounts) => {
-
-
-
+    // beforeEach(async function(){
+    //         this.contract = await contractname.new({from:owner})
+    //     })
     describe("Function - initGame", async ()=>{
 
         // let StateChGamingInstance = await StateChGaming.deployed();
@@ -76,6 +76,7 @@ contract('StateChGaming', async (accounts) => {
                 ,"can not have duplicate gameIDs"
             )
         })
+        it('shouldnt init a channel unless funds were withdrawn')
     })
     describe("Function - initBCMove", ()=>{
         it('should update the game blockNum and state')
@@ -85,77 +86,44 @@ contract('StateChGaming', async (accounts) => {
     })
 
     describe("Function - makeMoveTimed", ()=>{
-        it('')
+        it('should set the blockNumber')
     })
     describe("Function - makeMoveUntimed", ()=>{
-        it('')
+        it('should unset the blockNumber')
     })
     describe("Function - enforcedBCMove", ()=>{
-        it('')
+        it('should set state and blockNumber')
     })
     describe("Function - unenforcedBCMove", ()=>{
-        it('')
+        it('should set state and unset blockNumber')
     })
     describe("Function - payoutVictory", ()=>{
-        it('')
+        it('if p1 won, payout p1')
+        it('if p2 won, payout p2')
+        it('will revert if validating contract reverts')//HOW TO TEST THIS?
     })
     describe("Function - payoutTimeout", ()=>{
-        it('')
+        it('will revert if requested before min number of blocks hasnt passed')
+        it('will pay p1 if p1 played last')
+        it('will pay p2 if p2 played last')
     })
 
-
+    describe("Modifier - opponentLastPlayed via enforcedBCMove", ()=>{
+        it('allows p1 is p2 played last')
+        it('allows p2 if p1 played last')
+        it('doesnt allow p1 if p1 played last')
+        it('doesnt allow p2 if p2 played last')
+        it('doesnt allow anonymous address')
+    })
+    describe("Modifier - senderLastPlayed via makeMoveTimed", ()=>{
+        it('allows p1 is p1 played last')
+        it('allows p2 if p2 played last')
+        it('doesnt allow p2 if p1 played last')
+        it('doesnt allow p1 if p2 played last')
+        it('doesnt allow anonymous address')
+    })
+    describe("Modifier - validMove via enforcedBCMove", ()=>{
+        it('allows valid moves')
+        it('doesnt allow invalid moves')
+    })
 });
-
-// var ethers = require('ethers');
-
-// contract('CheckersArbitration', (accounts) => {
-
-//     let privKey1 = "64d3560a2d76391da1c1a44a9595102f0d7281f4ac5dbcefd3c1fe8f58f53be1"
-//     let addr1 = "0x20b2e1f1dc798951435234cb8a892f7483bd790e";
-//     let addr2 = accounts[1];
-//     let disputeContractAddr = accounts[7] ;//fix this later!
-
-//     it('use this to output state change tracking  ', async () => {
-       
-//         const CheckersArbitrationInstance = await CheckersArbitration.deployed();
-//         console.log((await CheckersArbitrationInstance.isForward(6,4)).toNumber())
-//     })
-
-
-//     // it('should create a channel if passed valid data and signature', async () => { 
-//     // })
-
-//     // group(MAIN CONTRACT)
-//     // group(function validateChannelSig) --> must be made public
-//     //     it('should return true when the signature corresponds to the data')
-//     //     it('should return true when the signature corresponds to the different data')
-//     //     it('should return false when the signature does not correspond to the data')
-
-//     // group(init channel)
-//     //     it('should create a channel if passed valid data and signature')
-//     //     it('should create a channel if passed different valid data and signature')
-//     //     it('should withdraw funds from both addresses)
-
-//     //     it('shouldn't create a channel if the gameID already exists)
-//     //     it('shouldn't create a channel if funds could not be withdrawn
-//     //     it('shouldn't take funds if the channel was not created)
-
-//     // group(')
-
-
-// });
-
-
-
-
-
-            // // ganache-cli -m "smart fun man behind sea joke split cherry force season once pair"
-            // let provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-            // let phrase = "smart fun man behind sea joke split cherry force season once pair"
-            // // let path = "m/44'/60'/0'/0/"
-            // // let secondwallet = ethers.Wallet.fromMnemonic(phrase, path + "1").connect(provider);
-            // let wallet = ethers.Wallet.fromMnemonic(phrase).connect(provider);
-
-            // let contractaddress = "0x59b8c37a34bc5f9ce80c71d5c6f307e698e6e20a"
-            // let contractabi = [{"constant":true,"inputs":[{"name":"salt","type":"uint256"}],"name":"deploy","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"salt","type":"uint256"}],"name":"Deployed","type":"event"}]
-            // let contract = new ethers.Contract(contractaddress,contractabi,wallet)
