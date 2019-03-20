fb9abe28593dceafeda2e894801b957470e959dd
MarkCBall <moball@hotmail.com>
2019-03-20 14:13:25 -0400

    getPrivateKey upgrade

--- solidity/Library/SigLib.js
+++ solidity/Library/SigLib.js
@@ -3,15 +3,10 @@ const ethers = require('ethers');
 
 // ganache-cli -m "smart fun man hind sea joke split cherry force season once pair"
 let getPrivKey = (accountNum) => {
-
-    //do this smarter
-//     Mnemonic:      smart fun man behind sea joke split cherry force season once pair
-// Base HD Path:  m/44'/60'/0'/0/{account_index}
-
-    if (accountNum === 0)
-        return "0x5ee6962f33f137e7847c8a2852ed18e5a67159f23b0931baf16a95a009ad3901"
-    if (accountNum === 1)
-        return "0x5ea9296aaa2bd9fa6089aa96f3b98b29b631180000f829f5979b9c472e286020"
+    let phrase = "smart fun man behind sea joke split cherry force season once pair"
+    let path = "m/44'/60'/0'/0/"
+    let wallet = ethers.Wallet.fromMnemonic(phrase, path+accountNum);
+    return wallet.signingKey.privateKey
 }
 
 module.exports = {
