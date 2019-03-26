e93cd9e8dfb397fc89e67033904a02f4c2b55f45
MarkCBall <moball@hotmail.com>
2019-03-25 23:38:07 -0400

    refactor privateKey change

--- frontend/src/redux/actions/LoginRedux.js
+++ frontend/src/redux/actions/LoginRedux.js
@@ -1,35 +1,15 @@
 import { CHANGE_ADDRESS_TEXT } from "../constants/LoginRedux";
-// import { SET_ACTIVE_CHANNEL } from "../constants/LoginRedux";
 import { HANDLE_PRIVKEY_CHANGE } from "../constants/LoginRedux";
-// 
 import { isValidAddress } from "ethereumjs-util";
-
-// import InteractDatabase from "./InteractDatabase";
-// import InteractBlockchain from "./InteractBlockchain";
-
-// //what does it mean to import itself??????? whoaaaa
-// import LoginRedux from "./LoginRedux";
 
 import { ethers } from "ethers";
 
 export default {
 
-    // renderChButtons: (dispatch, addressSignedIn) => {
-    //     return (dispatch) => {
-    //         dispatch(InteractDatabase.getPendingChannels(dispatch, addressSignedIn))
-    //         dispatch(InteractDatabase.getRequestedChannels(dispatch, addressSignedIn))
-    //         dispatch(InteractBlockchain.getOngoingChannels(dispatch, addressSignedIn))
-    //     }
-    // },
-
     handleAddressChange: (dispatch, addressSignedIn) => {
-        //clean up redundant returns?
         return (dispatch, getState) => {
             //set pubPrivKeypairValid according to logic
             if (/^0[xX][0-9a-fA-F]*$/.test(addressSignedIn)) {
-
-                //if isValid address && addressSignedIn===ethers.utils....
-                //dispatch with true 
 
                 let privKey = getState().LoginRedux.privKey
                 let pubPrivKeypairValid = (addressSignedIn === ethers.utils.computeAddress(privKey).toLowerCase())
@@ -41,54 +21,15 @@ export default {
                         pubPrivKeypairValid: pubPrivKeypairValid
                     }
                 })
-                // if (isValidAddress(addressSignedIn)){
-                //     dispatch(LoginRedux.renderChButtons(dispatch, addressSignedIn))
-                // }
             }
-            // dispatch({
-            //     type: SET_ACTIVE_CHANNEL,
-            //     payload: {channel:0}})
         }
     },
-    // setActiveChannel:(dispatch,channel, isOngoing) => {
-    //     return (dispatch) =>{
-    //         dispatch({
-    //             type: SET_ACTIVE_CHANNEL,
-    //             payload: {
-    //                 channel:channel,
-    //                 isOngoing:isOngoing
-    //             }
-    //         })
-    //     }
-    // },
+
 
     handlePrivKeyChange: (dispatch, privKeyText) => {
         return (dispatch) => {
-
-            //why doesn't it work do do the else clause always and let the change_address_text over-ride the pubPrivKeypairValid?
-
-            //change address text should dispatch get pending channels ect
-
-            if (privKeyText.length === 66) { //better validation to be done?
-                let correspondingPubAddress = ethers.utils.computeAddress(privKeyText).toLowerCase()
-                dispatch({
-                    type: CHANGE_ADDRESS_TEXT,
-                    payload: {
-                        addressSignedIn: correspondingPubAddress,
-                        addressIsValid: true,
-                        pubPrivKeypairValid: true
-                    }
-                })
-                //dispatch(LoginRedux.renderChButtons(dispatch, correspondingPubAddress))
-
-                dispatch({
-                    type: HANDLE_PRIVKEY_CHANGE,
-                    payload: {
-                        privKey: privKeyText,
-                        pubPrivKeypairValid: true
-                    }
-                })
-            } else {
+            if (/^0[xX][0-9a-fA-F]*$/.test(privKeyText)) {
+                //change the private key text
                 dispatch({
                     type: HANDLE_PRIVKEY_CHANGE,
                     payload: {
@@ -96,8 +37,27 @@ export default {
                         pubPrivKeypairValid: false
                     }
                 })
+                if (privKeyText.length === 66) {
+                    let correspondingPubAddress = ethers.utils.computeAddress(privKeyText).toLowerCase()
+                    dispatch({
+                        type: CHANGE_ADDRESS_TEXT,
+                        payload: {
+                            addressSignedIn: correspondingPubAddress,
+                            addressIsValid: true,
+                            pubPrivKeypairValid: true
+                        }
+                    })
+                    //reset the text indicating the key matches
+                    dispatch({
+                        type: HANDLE_PRIVKEY_CHANGE,
+                        payload: {
+                            privKey: privKeyText,
+                            pubPrivKeypairValid: true
+                        }
+                    })
+                } 
+
             }
-            // dispatch({type: SET_ACTIVE_CHANNEL,payload: {channel:0}})
         }
     },
 
