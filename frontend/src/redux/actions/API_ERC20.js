d8295a8d37ad90efa7bbc4e014c15978effdbf2d
MarkCBall <moball@hotmail.com>
2019-03-30 00:15:18 -0400

    uncommit comments

--- frontend/src/redux/actions/API_ERC20.js
+++ frontend/src/redux/actions/API_ERC20.js
@@ -10,26 +10,26 @@ export default {
 
     updateApprovals: () => {
         return async (dispatch, getState) => {
-        //     let GD = getState().GameData
-        //     let minStake = GD.ERC20Amount
-        //     let ERC20Addr = GD.ERC20Addr
-        //     let p1Addr = GD.p1Addr
-        //     let p2Addr = GD.p2Addr
-        //     let ERC20Contract = new ethers.Contract(ERC20Addr, ERC20Abi, provider)
-        //     let p1Apprv = (await ERC20Contract.allowance(p1Addr, StateChGamingAddr)).toString()
-        //     let p2Apprv = (await ERC20Contract.allowance(p2Addr, StateChGamingAddr)).toString()
-        //     let p1Bal = (await ERC20Contract.balanceOf(p1Addr)).toString()
-        //     let p2Bal = (await ERC20Contract.balanceOf(p2Addr)).toString()
-        //     dispatch({
-        //         type: UPDATE_APPROVALS,
-        //         payload: {
-        //             p1ApprovedAmnt: p1Apprv,
-        //             p2ApprovedAmnt: p2Apprv,
-        //             enoughAllowances: ((p1Apprv >= minStake) && (p2Apprv >= minStake)),
-        //             p1Bal: p1Bal,
-        //             p2Bal: p2Bal,
-        //         }
-        //     })
+            let GD = getState().GameData
+            let minStake = GD.ERC20Amount
+            let ERC20Addr = GD.ERC20Addr
+            let p1Addr = GD.p1Addr
+            let p2Addr = GD.p2Addr
+            let ERC20Contract = new ethers.Contract(ERC20Addr, ERC20Abi, provider)
+            let p1Apprv = (await ERC20Contract.allowance(p1Addr, StateChGamingAddr)).toString()
+            let p2Apprv = (await ERC20Contract.allowance(p2Addr, StateChGamingAddr)).toString()
+            let p1Bal = (await ERC20Contract.balanceOf(p1Addr)).toString()
+            let p2Bal = (await ERC20Contract.balanceOf(p2Addr)).toString()
+            dispatch({
+                type: UPDATE_APPROVALS,
+                payload: {
+                    p1ApprovedAmnt: p1Apprv,
+                    p2ApprovedAmnt: p2Apprv,
+                    enoughAllowances: ((p1Apprv >= minStake) && (p2Apprv >= minStake)),
+                    p1Bal: p1Bal,
+                    p2Bal: p2Bal,
+                }
+            })
         }
     },
 
